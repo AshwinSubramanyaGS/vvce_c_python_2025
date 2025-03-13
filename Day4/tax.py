4c5c99ea7aec680a6c513e12674a9bcaa1a0f3d1
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-03-13 11:33:27 +0530

    Hackthon

--- Day4/tax.py
+++ Day4/tax.py
@@ -1,2 +1,13 @@
 '''Program to accept name and salary. Check if their salary is >3L and display if they have to pay tax'''
 
+# Accept employee details
+name = input("Enter employee's name: ")
+salary = float(input("Enter employee's annual salary: "))
+
+# Check if salary is greater than 3,00,000
+if salary > 300000:
+    print(f"{name} has an annual salary of ₹{salary}.")
+    print(f"{name} has to pay taxes.")
+else:
+    print(f"{name} has an annual salary of ₹{salary}.")
+    print(f"{name} does not have to pay taxes.")
